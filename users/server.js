6b3e6d51fbf67283edb08cc88588e892cec0cf2d
for-alisia <for-alisia@yandex.ru>
2021-03-29 14:18:39 +0500

    Added schema to express server to handle users and companies

--- users/server.js
+++ users/server.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+
+/** Schema */
+const schema = require('./schema/schema');
 
 const app = express();
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    graphiql: true,
+    schema,
+  })
+);
 
 app.listen(4000, () => {
   console.log('Listenting on 4000');

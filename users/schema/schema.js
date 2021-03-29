0d61112313fc022f7971768a29bca80241d3602f
for-alisia <for-alisia@yandex.ru>
2021-03-29 14:59:09 +0500

    Added links between company and user

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -1,32 +1,38 @@
 const graphql = require('graphql');
 const axios = require('axios').default;
 
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema, GraphQLList } = graphql;
+
+const localPath = 'http://localhost:3000';
 
 const CompanyType = new GraphQLObjectType({
   name: 'Company',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
     description: { type: GraphQLString },
-  },
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(parentValue, args) {
+        return axios.get(`${localPath}/companies/${parentValue.id}/users`).then((res) => res.data);
+      },
+    },
+  }),
 });
 
 const UserType = new GraphQLObjectType({
   name: 'User',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
     company: {
       type: CompanyType,
       resolve(parentValue, args) {
-        return axios
-          .get(`http://localhost:3000/companies/${parentValue.companyId}`)
-          .then((res) => res.data);
+        return axios.get(`${localPath}/companies/${parentValue.companyId}`).then((res) => res.data);
       },
     },
-  },
+  }),
 });
 
 const RootQuery = new GraphQLObjectType({
@@ -36,7 +42,14 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return axios.get(`http://localhost:3000/users/${args.id}`).then((res) => res.data);
+        return axios.get(`${localPath}/users/${args.id}`).then((res) => res.data);
+      },
+    },
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return axios.get(`${localPath}/companies/${args.id}`).then((res) => res.data);
       },
     },
   },
